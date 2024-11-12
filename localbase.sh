b477d1b951e340c44efe9825c30d6d124022198b
Shunki Takami <tkmnet@users.noreply.github.com>
2024-11-12 17:33:34 +0900

    add gcd in localbash.sh

--- localbase.sh
+++ localbase.sh
@@ -2,3 +2,5 @@ set_localbase() { touch ".basedir.local"; }
 get_localbase() { sh -c 'lbd=""; while [ "" = "$lbd" -a "/" != "$(pwd)" ];do if [ -e ".basedir.local" ];then lbd=$(pwd); fi; cd ..; done; echo $lbd'; }
 lcd() { cd $(get_localbase); }
 
+get_gitbase() { sh -c 'lbd=""; while [ "" = "$lbd" -a "/" != "$(pwd)" ];do if [ -e ".git" ];then lbd=$(pwd); fi; cd ..; done; echo $lbd'; }
+gcd() { cd $(get_gitbase); }
